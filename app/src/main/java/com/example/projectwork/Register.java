3058128bac8b498f53efec83b0f40487804d4211
ahmadharthey <ahmoood@outlook.com>
2024-02-18 23:47:43 +0300

    completed registeration

--- app/src/main/java/com/example/projectwork/Register.java
+++ app/src/main/java/com/example/projectwork/Register.java
@@ -3,18 +3,37 @@ package com.example.projectwork;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
+    EditText instructorID;
+    EditText fullName;
+    EditText password;
+    EditText repeatPassword;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        instructorID = findViewById(R.id.instructorID_register);
+        fullName = findViewById(R.id.fullName);
+        password = findViewById(R.id.password_register);
+        repeatPassword = findViewById(R.id.repeatPassword);
     }
     public void handleNextRegister(View view){
         Intent nav = new Intent(this,RegisterFinal.class);
+        Bundle extras = new Bundle();
+        if(!password.getText().toString().equals(repeatPassword.toString())){
+            Toast.makeText(this, "make sure passwords match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        extras.putString("instructorID",instructorID.getText().toString());
+        extras.putString("fullName",fullName.getText().toString());
+        extras.putString("password",password.getText().toString());
+        nav.putExtras(extras);
         startActivity(nav);
     }
 }
--- app/src/main/java/com/example/projectwork/RegisterFinal.java
+++ app/src/main/java/com/example/projectwork/RegisterFinal.java
@@ -1,18 +1,70 @@
 package com.example.projectwork;
 
-import android.os.Bundle;
-import android.view.View;
+import static android.content.ContentValues.TAG;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterFinal extends AppCompatActivity {
+    DatabaseReference db;
+    EditText email_ET;
+    EditText DOB_ET;
+    EditText gender_ET;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_final);
+        email_ET = findViewById(R.id.email);
+        DOB_ET = findViewById(R.id.DOB);
+        gender_ET = findViewById(R.id.gender);
     }
     public void handeSendRegister(View view){
-        //todo handle register data and send it to firebase
+        Bundle extras=getIntent().getExtras();
+        String instructorID;
+        String fullName;
+        String password;
+        String email=email_ET.toString();
+        String DOB=DOB_ET.getText().toString();
+        String gender=gender_ET.getText().toString();
+        if(extras != null){
+            instructorID=extras.getString("instructorID");
+            fullName=extras.getString("fullName");
+            password=extras.getString("password");
+            db = FirebaseDatabase.getInstance().getReference("users");
+            Map<String, Object> data = new HashMap<>();
+            data.put("instructorID",instructorID);
+            data.put("fullName",fullName);
+            data.put("password",password);
+            data.put("email",email);
+            data.put("DOB",DOB);
+            data.put("gender",gender);
+            db.setValue(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(RegisterFinal.this, "saved!", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(RegisterFinal.this, "not saved", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, e.toString());
+                }
+            });
+        }
     }
 }
