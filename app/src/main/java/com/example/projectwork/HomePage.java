631248185bb34351514b518ac62e9920a9e12de6
Abdulaziz Alshamrani <2143980@uj.edu.sa>
2024-04-24 08:50:56 +0300

    added HomePage class

---
+++ app/src/main/java/com/example/projectwork/HomePage.java
@@ -0,0 +1,19 @@
+package com.example.projectwork;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+
+    public void handleGetStarted(View view) {
+
+    }
+}
