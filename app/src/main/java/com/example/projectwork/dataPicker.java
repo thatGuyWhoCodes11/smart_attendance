6f8a6da077dc7ca96e38e45b88ddaf47cdcc42b6
FarsMosh <2040849@uj.edu.sa>
2024-05-15 01:17:26 +0300

    log and date picker

---
+++ app/src/main/java/com/example/projectwork/dataPicker.java
@@ -0,0 +1,42 @@
+package com.example.projectwork;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText datePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datePicker = findViewById(R.id.date_picker);
+        ImageView calendarIcon = findViewById(R.id.ic_calendar);
+
+        calendarIcon.setOnClickListener(v -> showDatePickerDialog());
+        datePicker.setOnClickListener(v -> showDatePickerDialog());
+    }
+
+    private void showDatePickerDialog() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, year1, month1, dayOfMonth) -> {
+                    month1 += 1;
+                    String date = year1 + "/" + month1 + "/" + dayOfMonth;
+                    datePicker.setText(date);
+                }, year, month, day);
+        datePickerDialog.show();
+    }
+}
